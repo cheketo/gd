3dc70bd588f6bd8c567b51f117bff4c23ca18cbb
nanderson87 <nicolas.anderson87@gmail.com>
2017-11-03 23:27:40 -0700

    Crea un validador para validar la informacion de los campos

---
+++ proyecto/src/main/Core/Helpers/ValidadorHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgilFrba.Core.Helpers
+{
+    class ValidadorHelper
+    {
+        private static ValidadorHelper instancia;
+
+        private ValidadorHelper() { }
+
+        public static ValidadorHelper Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new ValidadorHelper();
+                }
+
+                return instancia;
+            }
+        }
+
+        public static bool ValidarTextBox(TextBox tb)
+        {
+            return !String.IsNullOrWhiteSpace(tb.Text);
+        }
+
+        public bool ValidarDateTimePicker(DateTimePicker dtp)
+        {
+            return (dtp.Value.ToString() != "");
+        }
+    }
+}
