6f0b1b08b41377e88e7deb71c85b1ce70a2b5982
nanderson87 <nicolas.anderson87@gmail.com>
2017-11-03 23:43:26 -0700

    Crea un controlador para controlar ciertos flujos comunes en la aplicacion

---
+++ proyecto/src/main/Core/Helpers/ControlHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgilFrba.Core.Helpers
+{
+    class ControlHelper
+    {
+
+        private static ControlHelper instancia;
+
+        private ControlHelper() { }
+
+        public static ControlHelper Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new ControlHelper();
+                }
+
+                return instancia;
+            }
+        }
+
+        public static void CerrarApp()
+        {
+            if (MensajeHelper.MostrarConfirmacion("¿Desea salir de PagoAgil?", "PagoAgil FRBA App") == DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        public static void CerrarSesionApp()
+        {
+            if (MensajeHelper.MostrarConfirmacion("¿Desea cerrar la sesión?", "PagoAgil FRBA App") == DialogResult.Yes)
+            {
+                Application.Restart();
+            }
+        }
+
+        public static void SolotNumerosEvento(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
