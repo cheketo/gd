7ff71fbf619387d5c4876f7d5ee429dc8a1fad52
nanderson87 <nicolas.anderson87@gmail.com>
2017-11-04 16:13:18 -0700

    Crea el modelo para la clase cliente

---
+++ proyecto/src/main/Core/ConexionDB/GD2C2017.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+namespace PagoAgilFrba
+{
+    public class GD2C2017 : DataContext
+    {
+        public Table<Cliente> Clientes;
+        public GD2C2017(string connection) : base(connection) { }
+    }
+}
---
+++ proyecto/src/main/Core/Modelo/Cliente.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagoAgilFrba.Core.ConexionDB;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+
+
+namespace PagoAgilFrba
+{
+    [Table(Name = "SQL_86.clientes")]
+    public class Cliente
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true)] public int Id { get; set; }
+        [Column] public string Dni { get; set; }
+        [Column] public string Apellido { get; set; }
+        [Column] public string Nombre { get; set; }
+        [Column(Name="fecha_nacimiento")] public DateTime FechaNacimiento { get; set; }
+        [Column] public string Mail { get; set; }
+        [Column] public string Direccion { get; set; }
+        [Column] public string CP { get; set; }
+        [Column] public string Piso { get; set; }
+        [Column] public string Depto { get; set; }
+        [Column] public string Telefono { get; set; }
+        [Column] public string Localidad { get; set; }
+    }
+}
