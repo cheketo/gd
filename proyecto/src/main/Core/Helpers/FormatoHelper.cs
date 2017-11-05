7a1b85f516b7f6223a1f3537640caa0ed6d96619
nanderson87 <nicolas.anderson87@gmail.com>
2017-11-05 00:24:01 -0700

    Crea FormatoHelper para manejar los formatos de los campos

---
+++ proyecto/src/main/Core/Helpers/FormatoHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PagoAgilFrba.Core.Helpers
+{
+    class FormatoHelper
+    {
+        private static FormatoHelper instancia;
+
+        private FormatoHelper() { }
+
+        public static FormatoHelper Instancia
+        {
+            get
+            {
+                if (instancia == null)
+                {
+                    instancia = new FormatoHelper();
+                }
+
+                return instancia;
+            }
+        }
+
+        public static char getEstadoFormatter(CheckBox cB)
+        {
+            if (cB.Checked)
+            {
+                return 'A';
+            }
+            else
+            {
+                return 'I';
+            }
+        }
+
+        public static Boolean setCheckedFromEstado(Char estado)
+        {
+            if (estado == 'A')
+            {
+                return true;
+            }
+                
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
