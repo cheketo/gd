c98d6891a59a0d2c3d5e47a4ec13d3e36150c254
nanderson87 <nicolas.anderson87@gmail.com>
2017-11-28 22:19:14 -0800

    Da estilo al formulario

--- proyecto/src/main/ListadoEstadistico/FormReporte.Designer.cs
+++ proyecto/src/main/ListadoEstadistico/FormReporte.Designer.cs
@@ -28,9 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FormReporte
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.Window;
+            this.ClientSize = new System.Drawing.Size(744, 611);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.DarkSlateGray;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormReporte";
+            this.Text = "Reportes - Pago Agil FRBA App";
+            this.ResumeLayout(false);
         }
 
         #endregion
